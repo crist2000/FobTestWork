350a89376aa755bee0542995caaae627f6ee1f22
crist2000 <mark.jurkin@gmail.com>
2017-03-01 10:21:55 +0200

    added possivility to start test as Main App
    added possivility to start test as Main App

--- TestWorkAId/src/main/java/some/domain/MainApp.java
+++ TestWorkAId/src/main/java/some/domain/MainApp.java
@@ -1,10 +1,37 @@
 package some.domain;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.testng.TestNG;
+import org.testng.xml.Parser;
+import org.testng.xml.XmlSuite;
+
+import some.domain.scenarios.Scenario1;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		TestNG testng = new TestNG();
+		String xmlFileName = "src/test/java/scenario1.xml";
+		List<XmlSuite> suite;
+		
+	    try {
+			suite = (List <XmlSuite>)(new Parser(xmlFileName).parse());
+		    testng.setXmlSuites(suite);
+		    testng.run();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+//		HowTo run specific test classes:
+//        TestNG testng = new TestNG();
+//        Class[] classes = new Class[]{Scenario1.class};
+//        testng.setTestClasses(classes);
+//        testng.run();
+		
 	}
 
 }
