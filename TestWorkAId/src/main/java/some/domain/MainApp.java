ba62ed47678edd2a198823b32bef3b910171186f
crist2000 <mark.jurkin@gmail.com>
2017-03-01 20:00:18 +0200

    Fixed test running from MainApp
    Fixed test running from MainApp. Configure build output folder for test
    classes to be the same as MainApp's ones to run the test from main().

--- TestWorkAId/src/main/java/some/domain/MainApp.java
+++ TestWorkAId/src/main/java/some/domain/MainApp.java
@@ -1,11 +1,35 @@
 package some.domain;
 
+import java.util.List;
 
+import org.testng.TestNG;
+import org.testng.xml.Parser;
+import org.testng.xml.XmlSuite;
+
+import some.domain.scenarios.Scenario1;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 
+		TestNG testng = new TestNG();
+		String xmlFileName = "src/test/java/scenario1.xml";
+		List<XmlSuite> suite;
+		
+	    try {
+			suite = (List <XmlSuite>)(new Parser(xmlFileName).parse());
+		    testng.setXmlSuites(suite);
+		    testng.run();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		//HowTo run specific test classes:
+//        TestNG testng = new TestNG();
+//        Class[] classes = new Class[]{Scenario1.class};
+//        testng.setTestClasses(classes);
+//        testng.run();
 		
 	}
 
